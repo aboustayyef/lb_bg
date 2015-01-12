049865a60541860e353459f23cf37b0a4ef86154
Mustapha Hamoui <mustapha.hamoui@gmail.com>
2015-01-12 13:03:04 +0000

    improved Category model

--- app/Bluegallery/Models/Category.php
+++ app/Bluegallery/Models/Category.php
@@ -5,6 +5,10 @@ class Category extends \Eloquent{
 	protected $table = "categories";
 	protected $fillable = ['id','name','image','description','parent_ID','slug'];
 
+    public function topLevel(){
+        return $this->where('parent_ID',0)->get();
+    }
+
     public function hasParent(){
         return ($this->parent_ID != 0);
     }
@@ -14,6 +18,15 @@ class Category extends \Eloquent{
             return $this->where('id', $this->parent_ID)->first();
         }
         return false;
+    }
+
+    public function children(){
+        $exists = $this->where('parent_Id', $this->id)->get();
+        if ($exists->count() == 0) {
+            return false;
+        } 
+        return $exists;
+        // categories only for now. Add products later
     }
 }
 
